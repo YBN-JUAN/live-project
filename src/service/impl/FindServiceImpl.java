4885fc9daaed92dcaae07c28669691364e67684f
86141 <861416687@qq.com>
2020-03-15 17:27:30 +0800

    查询接口类实现

---
+++ src/service/impl/FindServiceImpl.java
@@ -0,0 +1,14 @@
+package service.impl;
+
+import dao.RecordDAO;
+import dao.RecordDAOInterface;
+import pojo.Record;
+import service.FindService;
+
+public class FindServiceImpl implements FindService {
+    private RecordDAOInterface recordDAO=new RecordDAO();
+    @Override
+    public Record getRecord(int id) {
+        return recordDAO.getById(id);
+    }
+}
