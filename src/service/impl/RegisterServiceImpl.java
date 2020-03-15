65d3be22eb4725b68a75d552cf760447eee6800d
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-03-15 15:29:32 +0800

    登记服务类实现

---
+++ src/service/impl/RegisterServiceImpl.java
@@ -0,0 +1,24 @@
+package service.impl;
+
+import dao.RecordDAOInterface;
+import pojo.Record;
+import service.RegisterService;
+
+public class RegisterServiceImpl implements RegisterService {
+    private RecordDAOInterface dao=null;
+
+    @Override
+    public boolean canRegister(String userId, String phoneNumber, int orderId) {
+        return dao.ableOrder(orderId,userId,phoneNumber);
+    }
+
+    @Override
+    public int Register(String userId, String phoneNumber, int orderId, int maskNum) {
+        Record record=new Record();
+        record.setUserId(userId);
+        record.setTelNum(phoneNumber);
+        record.setOrderId(orderId);
+        record.setOrderNum(maskNum);
+        return dao.add(record).getId();
+    }
+}
