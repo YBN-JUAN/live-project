32785f1c800c669bc1f96c2a6592c0e39fbf1022
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-03-15 15:22:20 +0800

    接口返回类型修改

--- src/dao/OrderDAOInterface.java
+++ src/dao/OrderDAOInterface.java
@@ -3,8 +3,7 @@ package dao;
 import pojo.Order;
 
 public interface OrderDAOInterface {
-	
-	boolean add(int maskNum);
+	Order add(int maskNum);
 	Order get(int id);
 	int getMaskNum(int id);
 	boolean isOpening(int id);
--- src/dao/RecordDAOInterface.java
+++ src/dao/RecordDAOInterface.java
@@ -5,14 +5,11 @@ import java.util.List;
 import pojo.Record;
 
 public interface RecordDAOInterface {
-
-	boolean add(Record record);
+	Record add(Record record);
 	boolean setSelectedTrue(int id);
 	Record getById(int id);
 	Record getByUserID(String userId, int orderId);
 	Record getByTelNum(String telNum, int orderId);
 	List<Record> list(int orderId);
 	boolean ableOrder(int orderId, String userId, String telNum);
-	
-	
 }
