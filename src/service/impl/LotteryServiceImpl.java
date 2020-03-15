e9fc6d0e865746c44f9dfc51739c4395c5ec4b0b
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-03-15 17:06:44 +0800

    完善摇号服务实现

--- src/service/impl/LotteryServiceImpl.java
+++ src/service/impl/LotteryServiceImpl.java
@@ -1,12 +1,35 @@
 package service.impl;
 
+import dao.OrderDAO;
 import dao.OrderDAOInterface;
+import dao.RecordDAO;
+import dao.RecordDAOInterface;
+import pojo.Order;
+import pojo.Record;
 import service.LotteryService;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class LotteryServiceImpl  implements LotteryService {
-    private OrderDAOInterface orderDAOInterface =null;
+    private OrderDAOInterface orderDao =new OrderDAO();
+    private RecordDAOInterface recordDao=new RecordDAO();
     @Override
     public void endOrder(int orderId) {
-        orderDAOInterface.closeOrder(orderId);
+        Order order = orderDao.get(orderId);
+        int maskNum = order.getMaskNum();
+
+        List<Record> records = recordDao.list(orderId);
+        Collections.shuffle(records);
+        for(Record record:records){
+            if(maskNum>=record.getOrderNum()){
+                recordDao.setSelectedTrue(record.getId());
+                maskNum-=record.getOrderNum();
+            }else {
+                break;
+            }
+        }
+        orderDao.closeOrder(orderId);
     }
 }
