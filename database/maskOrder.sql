a95deaf4cae42965c23e662463781dd981037c19
SilverBay <wdrt68@foxmail.com>
2020-03-15 14:35:42 +0800

    创建数据库、准备JDBC驱动

---
+++ database/maskOrder.sql
@@ -0,0 +1,31 @@
+drop database if exists maskorder;
+create database maskorder;
+
+use maskorder;
+
+create table orders (
+
+id int auto_increment primary key, /*预约编号，表示第几次预约活动*/
+
+opening boolean default true, /*该次预约是否正在开放*/
+
+masknum int not null /*该次预约口罩数量*/
+);
+
+create table records (
+
+id int auto_increment primary key, /*记录编号*/
+
+orderid int, /*预约编号*/
+FOREIGN KEY(orderid) REFERENCES orders(id)
+	ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+userid varchar(20) not null, /*身份证号*/
+
+telnum varchar(20) not null, /*手机号*/
+
+ordernum int default 3, /*预约口罩数量*/
+
+selected boolean not null default false /*是否中签*/
+);
